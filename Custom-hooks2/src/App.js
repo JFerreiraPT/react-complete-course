5b0ec884d7b9107a9d24554d4cb3a873927b5a09
João Ferreira <joao.p.r.fereira.ofgmail.com>
2023-03-10 16:36:36 +0000

    feat: add code for custom hooks 2 projetc, add initial project for user inputs and forms

--- Custom-hooks2/src/App.js
+++ Custom-hooks2/src/App.js
@@ -1,43 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  const transformTasks = useCallback(tasksObj => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  }, []);
+
+  const {isLoading, error, sendRequest}  = useHttp(); 
+
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    sendRequest({
+      url: 'https://react-http-75b93-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  }, [sendRequest, transformTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
@@ -50,7 +36,7 @@ function App() {
         items={tasks}
         loading={isLoading}
         error={error}
-        onFetch={fetchTasks}
+        onFetch={sendRequest}
       />
     </React.Fragment>
   );
